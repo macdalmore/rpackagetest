c22608983f10c354a1fff540df4335982af2619c
sdaume <stefan.daume@gmail.com>
2018-02-28 18:56:00 +0100

    Add sample package object documentation
    This serves as a barebones example for creating package-level documentation. The most relevant tags have been included in this example, which is also intended as a self-documenting example, by listing relevant conventions.

---
+++ R/rpackagetest.R
@@ -0,0 +1,18 @@
+#' rpackagetest: A package to test the creation and maintenance of R packages
+#'
+#' As stated this is just a sandbox for experimenting with the different
+#' elements of R packages.
+#'
+#' @section Documentation options: Here too we can of course use all the
+#'   available object documentation \code{@@tags} - if applicable.
+#'
+#' @section Conventions: Here are some recommendations for documenting packages:
+#'   \itemize{ \item If provided, put the package documentation in file name
+#'   \emph{<package-name>.R}. \item It has to be labelled with \code{@@docType
+#'   package}. \item It must have a \code{@@name} specifying the package name.
+#'   \item Since it must not contain functions, it has to document \strong{NULL}
+#'   }
+#'
+#' @docType package
+#' @name rpackagetest
+NULL
