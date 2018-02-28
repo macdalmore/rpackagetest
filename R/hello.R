8af5f5c597353bf3e6af2909176e42ebc89aa2bf
sdaume <stefan.daume@gmail.com>
2018-02-28 18:36:49 +0100

    Add example for commonly used documentation tags
    This provides an additional function `hello_user` to demonstrate commonly used R object documentation tags. Naturally, this is a very simple example, but it serves as reference point for different uses of the various `@tags`.

--- R/hello.R
+++ R/hello.R
@@ -15,9 +15,41 @@
 
 #' Say hello.
 #'
+#' @family greetings
 #' @examples
 #' hello()
 hello <- function() {
   print("Hello, world!")
 }
 
+#' Greet a user
+#'
+#' \code{hello_user} returns a personalized greeting, using the provided
+#' \code{name} argument.
+#'
+#' This function serves as an example for showing the different options for
+#' documenting functions. Here you will find most of the \strong{@@tags}
+#' explained in the relevant chapter of the R package book found here:
+#' \url{http://r-pkgs.had.co.nz}.
+#'
+#' @section Customized link text: As a case in point the reference in the
+#'   previous sentence could also have been written with customized link text,
+#'   i.e. \href{http://r-pkgs.had.co.nz}{R package book}. And now we have also
+#'   demonstrated the use of a \code{@@section} tag.
+#'
+#' @param name A String of any length, specifying the user that should be greeted.
+#'
+#' @examples
+#' hello_user("A")
+#' hello_user("B")
+#'
+#' \dontrun{
+#' hello_user(A)
+#' }
+#'
+#' @family greetings
+#' @seealso \code{\link{hello}} which is in the same \code{@@family} of
+#'   functions, and of course check \url{http://r-pkgs.had.co.nz}.
+hello_user <- function(name) {
+  print(paste("Hello, ", name, "!", sep = ""))
+}
